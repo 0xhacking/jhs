13f874b22fd63d936b28f844ae2431d4f8f0c40f
Anjou <918873315@qq.com>
2018-12-19 12:05:09 +0800

    周分红_获取所有下级判定

--- plugin/commission/core/web/weekglons.php
+++ plugin/commission/core/web/weekglons.php
@@ -13,7 +13,8 @@ class Weekglons_EweiShopV2Page extends PluginWebPage
         $item = Array();
         $a = $this->get_member_id();
         $b = $this -> get_member_total();
-        $c = $this -> get_member_oneweek_total();
+//        $c = $this -> get_member_oneweek_total();
+        $c = $this -> get_member_down();
         for($i = 0 ; $i<count($this->get_member_id()) ; $i++){
             $item[$i] = $a[$i];
             $item[$i]["one_total"] = $b[$i];
@@ -37,6 +38,52 @@ class Weekglons_EweiShopV2Page extends PluginWebPage
         $g_m = pdo_fetchall('select id from '.tablename('ewei_shop_member').' where agentlevel = 10 or agentlevel = 12');
         return $g_m;
     }
+    //获取每个人的下级
+    public function get_member_down($id){
+        global $_W;
+        global $_GPC;
+        //获取初始符合条件的所有人
+        $g_m = $this -> get_member_id();
+
+        $a = Array();
+
+        //定义数组存储下级
+        $members_down = Array();
+        //判断此函数是否是第一次调取
+        if(empty($id)){
+            //此处做第一次计算，如果此函数为第一次调取
+            //获取id对应的一级下级
+            for($i = 0 ; $i<count($g_m);$i++){
+//               c empty(pdo_fetchall("select id from ".tablename('ewei_shop_member')." where jiedianrenid = ".$g_m[$i]["id"]))?0:1;
+                //$a[$i] = $g_m[$i]['id'];
+                $doen_cache = pdo_fetchall("select id from ".tablename('ewei_shop_member')." where jiedianrenid = ".$g_m[$i]["id"]);
+                if(!empty($doen_cache)){
+                    //$a[$i] = pdo_fetchall("select id from ".tablename('ewei_shop_member')." where jiedianrenid = ".$g_m[$i]["id"]);
+                    //查询到下级，拆分组装进新的数组
+                    for($w = 0 ;$w<count($doen_cache);$w++){
+                        $a[$g_m[$i]['id']][$w]["id"] = $doen_cache[$w]["id"];
+                        //$a[$i][$w]["p"] = $doen_cache[$w]["id"];
+                    }
+                }else{
+                    //无下级的id填0补位
+                    $a[$g_m[$i]['id']] = "0";
+                }
+            }
+        }else{
+            //此处做递归计算
+        }
+        //print_r($a);
+        show_json(1,$a);
+        return $a;
+
+    }
+
+
+
+
+
+
+
     //获取每个人的总业绩(当前用户)
     public function get_member_total(){
         global $_W;
@@ -50,47 +97,19 @@ class Weekglons_EweiShopV2Page extends PluginWebPage
         //show_json(1,$g_m_t);
         return $g_m_t;
     }
-    //获取每个人最近一周的业绩(当前用户)
-    public function get_member_oneweek_total(){
+    //获取某人最近一周的业绩(当前用户)
+    public function get_member_oneweek_total($id){
         global $_W;
         global $_GPC;
+
         $g_m = $this -> get_member_id();
         $g_m_w_t = Array();
-        $total = Array();
-        //
-//        for($i = 0 ; $i<count($g_m) ;$i++){
-//            $total = $this ->GetNewTotalformeByWeek($g_m[$i]['id']);
-//            array_push($g_m_w_t,$g_m[$i]['id']);
-//        }
-        //$r = $this->model->GetNewTotalformeByWeekByID('6565');
-        //获取当前用户的下级
-        $allmember = pdo_fetchall("select id from ".tablename("ewei_shop_member"). " where jiedianrenid = 6565 and isagent=1 and status=1 ");
-        //获取每个下级产生的业绩
-        for($t = 0 ; $t<count($allmember);$t++){
 
-        }
-
-        show_json(1,$this -> model->GetNewTotalformeByWeekByID('6572'));
+        //show_json(1,$g_m[2]['id']);
         return $g_m_w_t;
     }
 
-    public function GetNewTotalformeByWeek($angentid){
-        global $_W;
-        $totalforme = 0;
-        $member = m("member")->getMember($agentid);
-        if($member["weeknum"] == date('w')) return $member["weeksummoney"];
-        $allmember = pdo_fetchall("select openid from " .
-            tablename("ewei_shop_member") .
-            " where jiedianrenid =:agid and isagent=1 and status=1 ",array( ":agid" => $agentid));
-        $totalforme += $this->GetNewTotalformeByWeekByID($agentid);
-        	foreach ($allmember as $onemember) {
-        		$totalforme += this.GetNewTotalformeByWeek($onemember["id"]);
-        	}
-        pdo_update("ewei_shop_member",array("weekaddsum"=>$totalforme,"weeknum"=>date('w')),array("id"=>$agentid));
-        return $totalforme;
-    }
-
-    //获取每个人最近一周的业绩(非当前用户)
+    //获取每个人最近一周产生的业绩(非当前用户)(自购)
     public function get_member_oneweek_total_all(){
         global $_W;
         global $_GPC;
